aab3460e471da8030a862ffac4970850d2f31023
jussiheikkinen <jussih91@gmail.com>
2015-03-19 15:42:16 +0200

    UI

--- index.php
+++ index.php
@@ -9,21 +9,25 @@
 <body>
 
 <header>
+<div id="haku">
 <input style="text" value="Search" name="haku">
+</div>
 </header>
 
-<content id="sisalto">
-<h1>Admin Panel<h1>
-</content>
-
-<content id="sivupalkki">
+<div id="sivupalkki">
 <h2 id="otsikko">Contriboard</h2>
+
 <a href="url"><img src="view.png" alt="kuva" style="width:25px;height:25px">Boards</a><br>
 <a href="url"><img src="users.png" alt="kuva" style="width:25px;height:25px">Users</a><br>
 <a href="url"><img src="db.png" alt="kuva" style="width:25px;height:25px">Database</a><br>
 <a href="url"><img src="logout.png" alt="kuva" style="width:25px;height:25px">Log out</a>
-<p>users online<p>
-</content>
+<p>users online</p>
+</div>
+
+<div id="sisalto">
+<h1>Admin Panel</h1>
+</div>
+
 </body>
 
 </html>
