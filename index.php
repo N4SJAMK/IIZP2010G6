46873664970ddca95c1cd96e5276e06dcc45ec13
jussiheikkinen <jussih91@gmail.com>
2015-03-19 10:52:35 +0200

    UI

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Contriboard</title>
+<link rel="stylesheet" type="text/css" href="tyylit.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
