bebd36733ca7b3e11e1af15eed484c4e303bc75c
GitLeipuri <H3381@OHJE-322-WS-13.Labranet.jamk.fi>
2015-03-23 15:42:59 +0200

    nakyy collections

--- mongoDB.php
+++ mongoDB.php
@@ -4,12 +4,29 @@ Connection to mongo database
 */
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-try
-{
+try{	
     $m = new MongoClient(); // connect
 	echo '<br><pre>';
     print_r($m->listDBs());
 	echo '</pre>';
+	
+$m = new MongoClient();
+$db = $m->selectDB("teamboard-dev");
+$collections = $db->listCollections();
+
+foreach ($collections as $collection) {
+    echo "amount of documents in $collection: ";
+    echo $collection->count(), "\n<br>";
+}
+$m = new MongoClient();
+$db = $m->selectDB("local");
+$collections = $db->listCollections();
+
+foreach ($collections as $collection) {
+    echo "amount of documents in $collection: ";
+    echo $collection->count(), "\n<br>";
+}
+	
 }
 catch ( MongoConnectionException $e )
 {
