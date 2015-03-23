4d0caa50609e93e354b274d8eef30261c16607c7
GitLeipuri <H3381@OHJE-322-WS-13.Labranet.jamk.fi>
2015-03-23 16:51:19 +0200

    fix

--- auth.php
+++ auth.php
@@ -2,6 +2,6 @@
 session_start();
 if (!isset($_SESSION['app1_islogged']) || $_SESSION['app1_islogged'] !== true)
 	header("Location: http://" . $_SERVER['HTTP_HOST']
-                                    . dirname($_SERVER['PHP_SELF']) . '/'
+                                    . dirname($_SERVER['PHP_SELF']) . ''
                                     . 'login.php');
 ?>
