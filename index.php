2d33f1f1aba5005ba3301114ea1e775b0075415c
GitLeipuri <H3381@OHJE-322-WS-14.Labranet.jamk.fi>
2015-04-13 18:19:00 +0300

    asd

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 include 'auth.php';
 include 'head.php';
 ?>
-<h1>Admin Panel</h1>
-<input type="submit" value="Backup">
-<input type="submit" value="Restore">
+<h1>Boards</h1>
 <?php
-include("mongoDB.php");
+error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	try{	
+	$m = new MongoClient();
+	$db = $m->selectDB("teamboard-dev");
+	$cl = new MongoCollection($db, "boards");
+	$cursor = $cl->find();
+	echo '<pre>';
+	foreach ($cursor as $doc) {
+		print_r($doc);
+	}
+	echo '</pre>';
+
+
+}
+catch ( MongoConnectionException $e )
+{
+    echo '<p>Could not connect to MongoDB</p>';
+    exit();
+}
 include("foot.php");
 ?>
